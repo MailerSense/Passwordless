d8cf72fc5a0a57b6d721ba576d38f7c33cd9aa70
Marcin Praski <marcin.praski@live.com>
2025-04-10 07:29:56 +0200

    G

--- cdk/lib/application/public-ec2-app.ts
+++ cdk/lib/application/public-ec2-app.ts
@@ -115,17 +115,9 @@ export class PublicEC2App extends Construct {
       certificate,
       domainName: domain,
       domainZone: zone,
-      healthCheck: healthCheckCmd
-        ? {
-            command: healthCheckCmd,
-            interval: Duration.seconds(10),
-            timeout: Duration.seconds(5),
-            startPeriod: Duration.seconds(30),
-          }
-        : undefined,
       capacityProviderStrategies,
       containerMappingName,
-      containerMappingProtocol: AppProtocol.http2,
+      containerMappingProtocol: AppProtocol.http,
       serviceConnectConfiguration: namespace
         ? {
             namespace: namespace.namespaceName,
