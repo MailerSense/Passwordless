5e3e91e24687e5ef2c52ec88098f484060d8cc8f
Marcin Praski <marcin.praski@live.com>
2025-04-16 16:21:56 +0200

    G

--- lib/passwordless_web/live/app/embed_live/install.ex
+++ lib/passwordless_web/live/app/embed_live/install.ex
@@ -4,17 +4,30 @@ defmodule PasswordlessWeb.App.EmbedLive.Install do
   use PasswordlessWeb, :live_component
 
   alias Passwordless.App
-  alias Passwordless.AuthToken
   alias Passwordless.Repo
 
   @impl true
   def update(%{app: %App{} = app} = assigns, socket) do
     auth_token = Repo.preload(app, :auth_token).auth_token
-    signed = AuthToken.sign(auth_token)
+
+    keys = [
+      %{
+        id: "app_id",
+        name: gettext("App ID"),
+        token: app.id,
+        inserted_at: app.inserted_at
+      },
+      %{
+        id: "app_secret",
+        name: gettext("App Secret"),
+        token: "******",
+        inserted_at: auth_token.inserted_at
+      }
+    ]
 
     {:ok,
      socket
      |> assign(assigns)
-     |> assign(auth_token: auth_token, signed: signed)}
+     |> assign(keys: keys, auth_token: auth_token)}
   end
 end
