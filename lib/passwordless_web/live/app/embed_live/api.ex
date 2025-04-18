354f31e003662f9677eb2e0aaa72ddbd66dbf8f9
Marcin Praski <marcin.praski@live.com>
2025-04-18 13:37:52 +0200

    G

--- lib/passwordless_web/live/app/embed_live/api.ex
+++ lib/passwordless_web/live/app/embed_live/api.ex
@@ -3,8 +3,55 @@ defmodule PasswordlessWeb.App.EmbedLive.API do
 
   use PasswordlessWeb, :live_component
 
+  alias Passwordless.App
+
   @impl true
-  def update(assigns, socket) do
-    {:ok, assign(socket, assigns)}
+  def update(%{app: %App{} = app} = assigns, socket) do
+    changeset = Passwordless.change_app(app)
+
+    actions =
+      Enum.map(App.actions(), fn action ->
+        {Phoenix.Naming.humanize(action), action}
+      end)
+
+    icon_mapping = fn
+      nil -> "remix-checkbox-circle-fill"
+      "allow" -> "remix-checkbox-circle-fill"
+      :allow -> "remix-checkbox-circle-fill"
+      "block" -> "remix-close-circle-fill"
+      :block -> "remix-close-circle-fill"
+    end
+
+    {:ok,
+     socket
+     |> assign(assigns)
+     |> assign(
+       actions: actions,
+       icon_mapping: icon_mapping
+     )
+     |> assign_form(changeset)}
+  end
+
+  @impl true
+  def handle_event("validate", %{"app" => app_params}, socket) do
+    changeset =
+      socket.assigns.app
+      |> Passwordless.change_app(app_params)
+      |> Map.put(:action, :validate)
+
+    {:noreply, assign_form(socket, changeset)}
+  end
+
+  @impl true
+  def handle_event(_event, _params, socket) do
+    {:noreply, socket}
+  end
+
+  # Private
+
+  defp assign_form(socket, %Ecto.Changeset{} = changeset) do
+    socket
+    |> assign(form: to_form(changeset))
+    |> assign(default_action: Ecto.Changeset.get_field(changeset, :default_action))
   end
 end
