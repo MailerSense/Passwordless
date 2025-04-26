655e561720fa2cb95ad7a8e01a94360e2d242064
Marcin Praski <marcin.praski@live.com>
2025-04-26 18:40:37 +0200

    G

--- lib/passwordless_web/controllers/error_html.ex
+++ lib/passwordless_web/controllers/error_html.ex
@@ -15,75 +15,73 @@ defmodule PasswordlessWeb.ErrorHTML do
   # The default is to render a plain text page based on
   # the template name. For example, "404.html" becomes
   # "Not Found".
-  def render(template, %{reason: %Phoenix.Router.NoRouteError{plug_status: plug_status}} = assigns)
-      when is_integer(plug_status) do
-    assigns = Map.put(assigns, :message, Phoenix.Controller.status_message_from_template(template))
-
-    ~H"""
-    <.container class="flex flex-col items-center py-6 md:py-16" max_width="sm">
-      <.box card>
-        <.h3 no_margin class="text-center">
-          {"#{@status} #{@message}"}
-        </.h3>
-        <.p>
-          {gettext("Something went wrong: %{reason}", reason: @reason.message)}
-        </.p>
-        <div class="flex">
-          <.button link_type="a" title={gettext("Go to Homepage")} to={~p"/"} class="flex flex-1" />
-        </div>
-      </.box>
-    </.container>
-    """
-  end
-
   def render(template, %{status: status, message: message} = assigns) when is_integer(status) and is_binary(message) do
-    assigns = Map.put(assigns, :message, Phoenix.Controller.status_message_from_template(template))
+    assigns = Map.put(assigns, :status_message, Phoenix.Controller.status_message_from_template(template))
 
     ~H"""
-    <.container class="flex flex-col items-center py-6 md:py-16" max_width="sm">
-      <.box card>
-        <.h3 no_margin class="text-center">
-          {@status}
-        </.h3>
-        <.p>
-          {gettext("Something went wrong on our side. Please try again in a moment.")}
-        </.p>
-        <div class="flex">
-          <.button link_type="a" title={gettext("Go to Homepage")} to={~p"/"} class="flex flex-1" />
-        </div>
-      </.box>
-    </.container>
+    <.auth_layout title={gettext("%{status}: %{message}", status: @status, message: @status_message)}>
+      <:logo>
+        <.logo_icon class="w-20 h-20" />
+      </:logo>
+
+      <:top_links>
+        What's next?
+        <.link class="text-blue-600 underline dark:text-blue-400" navigate={~p"/"}>
+          {gettext("Back to Home")}
+        </.link>
+      </:top_links>
+
+      <.form_header title={gettext("Error details")} class="mb-6" />
+      <.p class="mb-6">
+        {@message}
+      </.p>
+      <div class="flex">
+        <.button link_type="a" title={gettext("Back to Home")} to={~p"/"} class="flex flex-1" />
+      </div>
+    </.auth_layout>
     """
   end
 
   def render(template, assigns) do
     assigns =
       assigns
-      |> Map.put(:path, get_in(assigns, [:conn, Access.key(:request_path)]) || "")
       |> Map.put(:status, get_in(assigns, [:conn, Access.key(:status)]) || 500)
       |> Map.put(:message, Phoenix.Controller.status_message_from_template(template))
+      |> Map.put(:reason_message, reason_message(assigns[:reason]))
 
     ~H"""
-    <.container class="flex flex-col items-center py-6 md:py-16" max_width="sm">
-      <.box card>
-        <.h3 no_margin class="text-center">
-          <span :if={@status} class={status_color(@status)}>{@status}</span> {@message}
-        </.h3>
-        <.p>
-          {gettext("Route %{path} not found.", path: @path)}
-        </.p>
-        <div class="flex">
-          <.button link_type="a" title={gettext("Go to Homepage")} to={~p"/"} class="flex flex-1" />
-        </div>
-      </.box>
-    </.container>
+    <.auth_layout title={gettext("%{status}: %{message}", status: @status, message: @message)}>
+      <:logo>
+        <.logo_icon class="w-20 h-20" />
+      </:logo>
+
+      <:top_links>
+        What's next?
+        <.link class="text-blue-600 underline dark:text-blue-400" navigate={~p"/"}>
+          {gettext("Back to Home")}
+        </.link>
+      </:top_links>
+
+      <.form_header title={gettext("Error details")} class="mb-6" />
+      <.p class="mb-6">
+        {@reason_message}
+      </.p>
+      <div class="flex">
+        <.button link_type="a" title={gettext("Back to Home")} to={~p"/"} class="flex flex-1" />
+      </div>
+    </.auth_layout>
     """
   end
 
   # Private
 
-  defp status_color(404), do: "text-primary-500"
-  defp status_color(status) when status in 400..499, do: "text-orange-500"
-  defp status_color(status) when status >= 500, do: "text-red-500"
-  defp status_color(_status), do: "text-slate-600 dark:text-slate-300"
+  defp reason_message(%mod{} = reason) do
+    if Kernel.function_exported?(mod, :message, 1) do
+      apply(mod, :message, [reason])
+    else
+      inspect(reason)
+    end
+  end
+
+  defp reason_message(_reason), do: ""
 end
