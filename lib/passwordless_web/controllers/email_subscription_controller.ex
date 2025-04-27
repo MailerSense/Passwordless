ef203333549bbaf958b9f2f09d36a3b89f1aae71
Marcin Praski <marcin.praski@live.com>
2025-04-27 13:31:51 +0200

    G

--- lib/passwordless_web/controllers/email_subscription_controller.ex
+++ lib/passwordless_web/controllers/email_subscription_controller.ex
@@ -3,15 +3,8 @@ defmodule PasswordlessWeb.EmailSubscriptionController do
 
   def unsubscribe(%Plug.Conn{} = conn, %{"token" => token}) when is_binary(token) do
     case Passwordless.unsubscribe_email(token, "one-click-post") do
-      {:ok, _} ->
-        conn
-        |> put_flash(:info, "You have been unsubscribed.")
-        |> redirect(to: "/")
-
-      {:error, _} ->
-        conn
-        |> put_flash(:error, "Invalid unsubscribe link!")
-        |> redirect(to: "/")
+      {:ok, _} -> conn |> put_status(200) |> json(%{status: 200})
+      {:error, _} -> conn |> put_status(400) |> json(%{status: 400})
     end
   end
 end
