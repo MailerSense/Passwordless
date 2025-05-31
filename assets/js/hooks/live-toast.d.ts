cc8432c3c35cc39f38da69f9e7ad4ed61436f503
Marcin Praski <marcin.praski@live.com>
2025-05-31 13:12:15 +0200

    G

--- assets/js/hooks/live-toast.d.ts
+++
@@ -1,7 +0,0 @@
-declare module "live_toast" {
-	import type { ViewHookInterface } from "phoenix_live_view";
-
-	const createLiveToastHook: () => ViewHookInterface;
-
-	export { createLiveToastHook };
-}
